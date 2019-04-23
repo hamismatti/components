586fe7bfdf44d61e0b5178b556a059bc00e1ca2e
Matti Hämäläinen <meh.hamalainen@gmail.com>
2019-04-24 01:21:01 +0300

    props addition

---
+++ src/ApprovalCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ApprovalCard = () => {
+	return (
+		<div className="ui card">
+			<div className="content">
+				Are you sure?
+				<div className="extra content">
+					<div className="ui two buttons" />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ApprovalCard;
